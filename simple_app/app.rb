7a63a3ea55dae3bba30baad5aed1c450083a09ff
Manas Gupta <manas.gupta@gmail.com>
2013-03-16 07:17:49 +0000

    Simple hello world app. Slow in responding sometime

--- simple_app/app.rb
+++ simple_app/app.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'sinatra'
 
+def be_slow 
+    # current time, if second is even - be slow!
+    return (Time.now.sec % 2 == 0)
+end
+
 get "/" do
-    "hi"
+    if be_slow 
+        sleep 2
+        "I am feeling slow"
+    else
+        "Hi as usual!"
+    end
 end
