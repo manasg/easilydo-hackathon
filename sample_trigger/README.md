fd7f8d25835970545e6c5ba5da2a45b6d2d9e0e1
Manas Gupta <manas.gupta@gmail.com>
2013-03-16 10:28:52 +0000

    Trigger simulator

---
+++ sample_trigger/README.md
@@ -0,0 +1,6 @@
+Boilerplate code to test Easilydo 
+
+ImportError: No module named requests
+Try :-
+  sudo apt-get install python-pip
+  sudo pip install requests
---
+++ sample_trigger/test.py
@@ -0,0 +1,33 @@
+import os
+import base64
+import requests
+import json
+
+POST_TASK_URL = "https://agent8-backend-staging.appspot.com/hackathon/discovery/postTask"
+
+def run(request=None):
+    # Handle the request
+
+    # Discovery algorithms go here, 
+    # such as discovering if you have recently added a new friend in Facebook
+
+    # POST data from trigger code to Do-Engine
+    payload = {
+                "doType" : 5505, # ID of the Trigger in EasilyDo Builder
+                "doResponse" : [{
+                    "userName" : "manas@easilydo.com", # EasilyDo user name
+                    "uniqueId" : base64.urlsafe_b64encode(os.urandom(30)), 
+                    "variables" : {
+                        "instance_id" : "i-999999999",
+                        "server_name" : "webserver-99"
+                        }
+                    }]  
+                }
+    r = requests.post(POST_TASK_URL, data=json.dumps(payload))
+    print r
+    print 'Found new friend, hello EasilyDo'
+    return True
+    
+
+if __name__ == '__main__':
+    run()
